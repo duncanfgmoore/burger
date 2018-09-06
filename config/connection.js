de3378ed760e0ddd5cb153769fc60b7822cd977d
Duncan <duncan@stp-er-10-129-157-250.wireless.umn.edu>
2018-09-05 21:34:35 -0500

    jfla

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,10 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
+  console.log("******jaws")
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
+  console.log("*******local")
   connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
