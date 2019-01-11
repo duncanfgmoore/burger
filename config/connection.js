9d06e31b94c58e9d577dc2ede73023bf91d83179
duncanfgmoore <duncanfgmoore@gmail.com>
2019-01-11 10:01:46 -0600

    cleaning up code

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,20 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  console.log("******jaws")
+  console.log("******jaws");
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  console.log("*******local")
+} else {
+  console.log("*******local");
   connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db"
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
   });
-};
+}
 
-connection.connect(function (err) {
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- config/orm.js
+++ config/orm.js
@@ -4,7 +4,7 @@ function printQuestionMarks(num) {
   var arr = [];
 
   for (var i = 0; i < num; i++) {
-      arr.push("?");
+    arr.push("?");
   }
 
   return arr.toString();
@@ -16,33 +16,33 @@ function objToSql(ob) {
 
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-          // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-          if (typeof value === "string" && value.indexOf(" ") >= 0) {
-              value = "'" + value + "'";
-          }
-          
-          arr.push(key + "=" + value);
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
       }
+
+      arr.push(key + "=" + value);
+    }
   }
   return arr.toString();
 }
 
 var orm = {
-  selectAll: function (table, callback) {
-    connection.query("SELECT * FROM ??;", [table], function (err, result) {
-        if (err) {
-            throw err;
-        }
-        console.log(result);
-        callback(result);
-        //result.render("index", { plans: data });
+  selectAll: function(table, callback) {
+    connection.query("SELECT * FROM ??;", [table], function(err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+      callback(result);
+      //result.render("index", { plans: data });
     });
-},
+  },
 
-  insertOne: function (table, cols, vals, callback) {
+  insertOne: function(table, cols, vals, callback) {
     var queryString = "INSERT INTO " + table;
     queryString += "(";
     queryString += cols.toString();
@@ -53,22 +53,22 @@ var orm = {
 
     console.log(queryString);
 
-    connection.query(queryString, vals, function (err, result) {
-        if (err) throw err;
-        callback(result);
+    connection.query(queryString, vals, function(err, result) {
+      if (err) throw err;
+      callback(result);
     });
   },
 
-  updateOne: function (table, objColVals, condition, callback) {
+  updateOne: function(table, objColVals, condition, callback) {
     var queryString = "UPDATE " + table;
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE " + condition;
-        console.log(queryString);
-        connection.query(queryString, function (err, result) {
-            if (err) throw err;
-            callback(result);
-        });
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE " + condition;
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      callback(result);
+    });
   }
 };
 
