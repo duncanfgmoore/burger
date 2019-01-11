dbbb395ce600ceaea25ee69a7fc344e53c53b7c2
duncanfgmoore <duncanfgmoore@gmail.com>
2019-01-11 10:01:00 -0600

    cleaning up code

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,7 +3,7 @@ var burger = require("../models/burgers.js");
 var router = express.Router();
 
 //Create a controller that will GET all the burgers that are in the database
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
@@ -13,34 +13,32 @@ router.get("/", function (req, res) {
 });
 
 //Create a controller that will POST new burgers to the database
-router.post("/api/burgers", function (req, res) {
- 
-    burger.insertOne(["burger_name"
-  ], [
-      req.body.burger_name
-  ], function(result) {
-      res.json({ id: result.insertId });
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne(["burger_name"], [req.body.burger_name], function(result) {
+    res.json({ id: result.insertId });
   });
 });
-  
+
 //Create a controller that will update devoured to true in the database
-router.put("/api/burgers/:id", function (req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
+  burger.updateOne(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
     }
-  });
+  );
 });
-
-
 
 module.exports = router;
