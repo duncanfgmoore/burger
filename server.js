de7795648ab782dea256a3fb9f1620986b92c947
duncanfgmoore <duncanfgmoore@gmail.com>
2019-01-11 10:00:45 -0600

    cleaning up code

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-
 app.use(express.static("public"));
 
-
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Set Handlebars.
